e3b818930d3d2e24853428afd136cf0b39a1affd
keoghpe@tcd.ie <keoghpe@tcd.ie>
2015-05-26 15:00:52 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+// To do:
+// grunt sass
+// bootstrap
+// livereload
+// wiredep
+// 
+
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'js/**/*.js', 'js/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint']);
+
+};
